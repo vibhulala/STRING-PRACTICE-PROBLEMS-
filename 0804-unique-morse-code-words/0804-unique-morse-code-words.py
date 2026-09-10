2181dd488918f267e5259d930cedb8623951634f
vibhulala <bitwisenilay2006@gmail.com>
2026-09-10 22:32:27 +0530

    Time: 4 ms (7.37%), Space: 19.4 MB (5.35%) - LeetHub

--- 0804-unique-morse-code-words/0804-unique-morse-code-words.py
+++ 0804-unique-morse-code-words/0804-unique-morse-code-words.py
@@ -1,5 +1,9 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        '''
+        bruet froce:- isme pehle morse code ka main list bana diya fir ek fianl result sto ke liye unique[] bbana then har word ko character by charcter traverse akrke mrse me badla agr unique nahi rha to append akr denege unique maien and retunr kar deneg uniwue ki length isme thoda duplicates ko do bar traverse akrn apad rha 
+        tc-o(n^2+nxk)
+        sc-o(nxk)
         morse = [
             ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
             "....", "..", ".---", "-.-", ".-..", "--", "-.",
@@ -14,4 +18,19 @@ class Solution:
                 code+=morse[index]
             if code not in unique:
                 unique.append(code)
+        return len(unique)
+        '''
+        morse = [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
+            "....", "..", ".---", "-.-", ".-..", "--", "-.",
+            "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+            "...-", ".--", "-..-", "-.--", "--.."
+        ]
+        unique=set()
+        for word in words:
+            code=[]
+            for ch in word:
+                index=ord(ch)-ord('a')
+                code.append(morse[index])
+            unique.add("".join(code))
         return len(unique)
