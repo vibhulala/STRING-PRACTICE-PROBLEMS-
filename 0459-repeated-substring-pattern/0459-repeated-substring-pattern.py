006a2510d63650743b7f4c4e57268422bf3c2473
vibhulala <bitwisenilay2006@gmail.com>
2026-09-11 00:59:52 +0530

    Time: 91 ms (11.28%), Space: 19.3 MB (34.19%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,5 +1,8 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
+        '''
+        brute force :- isme hmne pehle diye hue string kki length calculate kar li use bad length to length to ek loop chalya length 1 ayi to fir n% length agr valid hua to utn hamne pattern banaay aur repeat kara ay agr repeated wala original wale str s ke barabb ho gya to bat ban gayi return tru nhai to return false
+
         n=len(s)
         for length in range(1,n):
             #string must be completely divisible 
@@ -9,3 +12,28 @@ class Solution:
                 if pattern*repetitions==s:
                     return True 
         return False 
+        time comppexity:-o(n^2)
+        space complexity:-o(n)
+        '''
+        n = len(s)
+
+        for length in range(1, n):
+
+            # Pattern length must divide string length
+            if n % length != 0:
+                continue
+
+            valid = True
+
+            # Compare every character with repeating pattern
+            for i in range(length, n):
+
+                if s[i] != s[i % length]:
+                    valid = False
+                    break
+
+            if valid:
+                return True
+
+        return False
+        
